82a1865606151148e517253935922c00d879b286
SandeepaDevin <Sandeepdevin@gmail.com>
2023-05-09 23:16:57 +0530

    changed color

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,8 +3,8 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-background: #331af0;
-height: 85px;
+background: #ff6900;
+height: 90px;
 display: flex;
 justify-content: space-between;
 padding: 0.2rem calc((100vw - 1000px) / 2);
